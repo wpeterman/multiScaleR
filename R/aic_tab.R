fdd33d70a1fadf139598a8764d87c4d4c4d8eb5c
Bill Peterman <peterman.73@osu.edu>
2026-05-06 20:30:59 -0400

    Fix aic_tab documentation registration

--- R/aic_tab.R
+++ R/aic_tab.R
@@ -1,86 +1,3 @@
-# AIC table ---------------------------------------------------------------
-#' @author Bill Peterman
-#' @title multiScaleR model selection
-#' @description Function to create AIC(c) table of fitted models
-#' @param mod_list List containing fitted `multiScaleR` objects
-#' @param AICc Use second order AIC in ranking models (Default = TRUE). See Details
-#' @param mod_names Optional. Specify names for fitted model objects. By default, the right hand side of the fitted `multiScaleR` model, in combination with the kernel, will be used as the model name.
-#' @param verbose (Default = FALSE) Should the table be printed to the console
-#' @param ... Additional arguments (Not used)
-#'
-#' @return Data frame of class `aictab` with AIC summary table for provided models
-#' @export
-#'
-#' @details
-#' aic_tab creates a model selection table using \code{\link[AICcmodavg]{aictabCustom}} from the `AICcmodavg` package
-#'
-#' @usage
-#' aic_tab(mod_list,
-#'         AICc = TRUE,
-#'         mod_names = NULL,
-#'         verbose = FALSE,
-#'         ...)
-#' @rdname aic_tab
-#' @importFrom insight find_formula get_loglikelihood get_parameters n_obs
-#' @importFrom AICcmodavg aictabCustom bictabCustom
-#'
-#' @examples
-#' ## Simulate data
-#' set.seed(555)
-#'
-#' points <- vect(cbind(c(5,7,9,11,13),
-#'                      c(13,11,9,7,5)))
-#'
-#' mat_list <- list(r1 = rast(matrix(rnorm(20^2),
-#'                                   nrow = 20)),
-#'                  r2 = rast(matrix(rnorm(20^2),
-#'                                   nrow = 20)))
-#' rast_stack <- rast(mat_list)
-#' kernel_inputs <- kernel_prep(pts = points,
-#'                              raster_stack = rast_stack,
-#'                              max_D = 5,
-#'                              kernel = 'gaussian',
-#'                              sigma = NULL)
-#' ## Example response data
-#' y <- rnorm(5)
-#'
-#' ## Create data frame with raster variables
-#' dat <- data.frame(y = y,
-#'                   kernel_inputs$kernel_dat)
-#' mod1 <- glm(y ~ r1,
-#'             data = dat)
-#' mod2 <- glm(y ~ r2,
-#'             data = dat)
-#' mod3 <- glm(y ~ r1 + r2,
-#'             data = dat)
-#'
-#' ## NOTE: This code is only for demonstration
-#' ## Optimization results will have no meaning
-#'
-#' opt_mod1 <- multiScale_optim(fitted_mod = mod1,
-#'                              kernel_inputs = kernel_inputs,
-#'                              par = NULL,
-#'                              n_cores = NULL)
-#'
-#' opt_mod2 <- multiScale_optim(fitted_mod = mod2,
-#'                              kernel_inputs = kernel_inputs,
-#'                              par = NULL,
-#'                              n_cores = NULL)
-#' opt_mod3 <- multiScale_optim(fitted_mod = mod3,
-#'                              kernel_inputs = kernel_inputs,
-#'                              par = NULL,
-#'                              n_cores = NULL)
-#'
-#'  ## AIC table
-#'  mod_list <- list(opt_mod1, opt_mod2, opt_mod3)
-#'
-#'  aic_tab(mod_list = mod_list,
-#'          AICc = FALSE)
-#'
-#'  ## AICc table with specified names
-#'  aic_tab(mod_list = mod_list,
-#'          AICc = TRUE,
-#'          mod_names = c('mod1', 'mod2', 'mod3'))
 .msr_selection_model <- function(model) {
   if (inherits(model, "multiScaleR")) {
     return(model$opt_mod)
@@ -239,6 +156,83 @@
 }
 
 
+# AIC table ---------------------------------------------------------------
+#' @author Bill Peterman
+#' @title multiScaleR model selection
+#' @description Function to create AIC(c) table of fitted models
+#' @param mod_list List containing fitted `multiScaleR` objects
+#' @param AICc Use second order AIC in ranking models (Default = TRUE). See Details
+#' @param mod_names Optional. Specify names for fitted model objects. By default, the right hand side of the fitted `multiScaleR` model, in combination with the kernel, will be used as the model name.
+#' @param verbose (Default = FALSE) Should the table be printed to the console
+#' @param ... Additional arguments (Not used)
+#'
+#' @return Data frame of class `aictab` with AIC summary table for provided models
+#' @export
+#'
+#' @details
+#' aic_tab creates a model selection table using \code{\link[AICcmodavg]{aictabCustom}} from the `AICcmodavg` package
+#'
+#' @rdname aic_tab
+#' @importFrom insight find_formula get_loglikelihood get_parameters n_obs
+#' @importFrom AICcmodavg aictabCustom bictabCustom
+#'
+#' @examples
+#' ## Simulate data
+#' set.seed(555)
+#'
+#' points <- vect(cbind(c(5,7,9,11,13),
+#'                      c(13,11,9,7,5)))
+#'
+#' mat_list <- list(r1 = rast(matrix(rnorm(20^2),
+#'                                   nrow = 20)),
+#'                  r2 = rast(matrix(rnorm(20^2),
+#'                                   nrow = 20)))
+#' rast_stack <- rast(mat_list)
+#' kernel_inputs <- kernel_prep(pts = points,
+#'                              raster_stack = rast_stack,
+#'                              max_D = 5,
+#'                              kernel = 'gaussian',
+#'                              sigma = NULL)
+#' ## Example response data
+#' y <- rnorm(5)
+#'
+#' ## Create data frame with raster variables
+#' dat <- data.frame(y = y,
+#'                   kernel_inputs$kernel_dat)
+#' mod1 <- glm(y ~ r1,
+#'             data = dat)
+#' mod2 <- glm(y ~ r2,
+#'             data = dat)
+#' mod3 <- glm(y ~ r1 + r2,
+#'             data = dat)
+#'
+#' ## NOTE: This code is only for demonstration
+#' ## Optimization results will have no meaning
+#'
+#' opt_mod1 <- multiScale_optim(fitted_mod = mod1,
+#'                              kernel_inputs = kernel_inputs,
+#'                              par = NULL,
+#'                              n_cores = NULL)
+#'
+#' opt_mod2 <- multiScale_optim(fitted_mod = mod2,
+#'                              kernel_inputs = kernel_inputs,
+#'                              par = NULL,
+#'                              n_cores = NULL)
+#' opt_mod3 <- multiScale_optim(fitted_mod = mod3,
+#'                              kernel_inputs = kernel_inputs,
+#'                              par = NULL,
+#'                              n_cores = NULL)
+#'
+#' ## AIC table
+#' mod_list <- list(opt_mod1, opt_mod2, opt_mod3)
+#'
+#' aic_tab(mod_list = mod_list,
+#'         AICc = FALSE)
+#'
+#' ## AICc table with specified names
+#' aic_tab(mod_list = mod_list,
+#'         AICc = TRUE,
+#'         mod_names = c('mod1', 'mod2', 'mod3'))
 aic_tab <- function(mod_list,
                     AICc = TRUE,
                     mod_names = NULL,
